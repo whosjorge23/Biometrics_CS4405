12d72dc9d44c13793768c9397f00fc528c7dea8c
Giorgio Giannotta <jorge_upa@msn.com>
2024-12-18 09:22:12 +0700

    Initial Commit

---
+++ Biometrics_CS4405/Biometrics_CS4405App.swift
@@ -0,0 +1,17 @@
+//
+//  Biometrics_CS4405App.swift
+//  Biometrics_CS4405
+//
+//  Created by Giorgio Giannotta on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Biometrics_CS4405App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
